1df186a6621b8803957373fbbb6b2594f084057b
Austin Ameh <arewageek@gmail.com>
2024-09-12 03:17:26 +0100

    Carousel integrated (with build errors)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,6 @@ import "./globals.css";
 import AppBar from "../components/layouts/AppBar";
 import Footer from "@/components/layouts/Footer";
 
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
 const inter = Inter({ subsets: ["latin"] });
 const montserrat = Montserrat({ style: "normal", subsets: ["cyrillic"] })
 const onest = Onest({ style: "normal", subsets: ["cyrillic"] })
--- src/components/Public/CarsSlide.tsx
+++ src/components/Public/CarsSlide.tsx
@@ -1,5 +1,8 @@
+"use client"
+
 import Image from 'next/image'
 import React from 'react'
+import Carousel, { ReactElasticCarouselProps } from 'react-elastic-carousel'
 
 interface Cars {
     name: string
@@ -7,12 +10,24 @@ interface Cars {
 
 const cars: Cars[] = [
     { name: "Rolls Roys Cullinum" },
-    { name: "Toyota Camry" }
+    { name: "Toyota Camry" },
+    { name: "Venza" },
+    { name: "Honda Camry" },
+    { name: "Toyota" },
 ]
+
+const options: ReactElasticCarouselProps = {
+    isRTL: false,
+    autoPlaySpeed: 3000,
+    enableAutoPlay: true,
+    itemsToShow: 2,
+    enableSwipe: true,
+    showEmptySlots: true
+}
 
 const CarsSlide = () => {
     return (
-        <div className='py-10 lg:py-24 px-5 hidden lg:block'>
+        <div className='py-10 lg:py-24 px-5 hidden lg:block w-full'>
             <div className='text-center'>
                 <span className='text-accent-100 text-[9pt] tracking-widest font-medium'>
                     SELECT YOUR CAR
@@ -24,21 +39,27 @@ const CarsSlide = () => {
                 </h2>
             </div>
 
-            <div className='flex overflow-x-auto gap-3 mt-6 min-h-[500px] px-5 h-auto relative'>
-                {cars.map(car => {
-                    return <div key={car.name} className='group relative h-full'>
-                        <div className='relative h-full'>
-                            <div className='overflow-hidden bg-red-500 lg:w-[800px] w-full h-auto lg:h-[400px] rounded-3xl '>
-                                <Image src="/12.jpg" height="8000" width="4000" alt="car 12" className='w-full h-full group-hover:scale-[110%] transition duration-300' />
-                            </div>
-                            <div className='absolute -bottom-[40px] py-4 w-[80%] left-[10%] rounded-3xl bg-base-300 shadow flex items-center justify-between px-10'>
-                                <h2 className='text-white text-2xl font-bold'>
-                                    {car.name}
-                                </h2>
+            <div className='flex overflow-x-auto gap-3 mt-6 min-h-[500px] px-5 h-auto relative w-full'>
+
+
+                <Carousel {...options} className='w-full'>
+                    {cars.map(car => {
+                        return <div key={car.name} className='group relative h-full w-full'>
+                            <div className='relative h-full'>
+                                <div className='p-4'>
+                                    <div className='overflow-hidden bg-[url("/12.jpg")] bg-center bg-cover hover:scale-110 transis w-full h-[400px] rounded-3xl p-5'>
+                                        {/* <Image src="/12.jpg" height="8000" width="4000" alt="car 12" className='w-full h-full group-hover:scale-[110%] transition duration-300' /> */}
+                                    </div>
+                                </div>
+                                <div className='absolute -bottom-[40px] py-4 w-[80%] left-[10%] rounded-3xl bg-base-300 shadow flex items-center justify-between px-10'>
+                                    <h2 className='text-white text-2xl font-bold'>
+                                        {car.name}
+                                    </h2>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                })}
+                    })}
+                </Carousel>
             </div>
         </div>
     )
