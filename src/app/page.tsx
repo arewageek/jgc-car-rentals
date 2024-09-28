261d72a578aae68c55c0fbbf872652554858f678
Austin Ameh <arewageek@gmail.com>
2024-09-28 15:55:32 +0100

    Car carousel removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import Image from "next/image";
 import HeroSection from "../components/layouts/HeroSection";
 import AboutUs from "../components/Public/AboutUs";
-import { CgArrowLongDown } from "react-icons/cg";
-import CarsSlide from "../components/Public/CarsSlide";
 import RentalProcess from "../components/Public/RentalProcess";
 import CarVideoSection from "@/components/Public/CarVideoSection";
-import Partners from "@/components/Public/Partners";
 import ContactBlock from "@/components/Public/ContactBlock";
 
 export default function Home() {
@@ -13,10 +9,8 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between">
       <HeroSection />
       <AboutUs />
-      {/* <CarsSlide /> */}
       <RentalProcess />
       <CarVideoSection />
-      {/* <Partners /> */}
       <ContactBlock />
     </main>
   );
